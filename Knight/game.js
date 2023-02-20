19a2aa34bb283fac825daeb2cfe1b0ca77855ff0
ChimentiMatt <chimcode@gmail.com>
2023-02-20 14:17:53 -0800

    animations to run right, idle and attack working

--- Knight/game.js
+++ Knight/game.js
@@ -10,7 +10,7 @@ function init() {
 	h = stage.canvas.height;
 
 	manifest = [
-        {src: "runandattack.png", id: "knight"},
+        {src: "sprite.png", id: "knight"},
 	];
 
 	loader = new createjs.LoadQueue(false);
@@ -23,39 +23,40 @@ function handleComplete() {
 
 	var spriteSheet = new createjs.SpriteSheet({
 			images: [loader.getResult("knight")],
-            framerate: 15,
+            framerate: 5,
 			frames: [
-				[1, 1, 63, 67, 0, -15, -10],
-				[1, 70, 56, 46, 0, -7, -19],
-				[59, 70, 37, 46, 0, -32, -19],
-				[66, 1, 56, 65, 0, -32, 0],
-				[98, 68, 56, 47, 0, -7, -18],
-				[124, 1, 37, 62, 0, -30, -3],
-				[156, 65, 36, 51, 0, -25, -16],
-				[163, 1, 34, 51, 0, -27, -17],
-				[194, 54, 34, 51, 0, -27, -16],
-				[199, 1, 34, 51, 0, -27, -15],
-				[230, 54, 32, 51, 0, -29, -18],
-				[235, 1, 31, 51, 0, -31, -18],
-				[264, 54, 31, 51, 0, -30, -17],
-				[268, 1, 30, 51, 0, -31, -17],
-				[300, 1, 37, 50, 0, -30, -15],
-				[300, 53, 37, 50, 0, -30, -15]
+				[0, 0, 30, 51, 0, -31, -17],
+				[30, 0, 31, 51, 0, -31, -18],
+				[61, 0, 31, 51, 0, -30, -17],
+				[92, 0, 32, 51, 0, -29, -18],
+				[0, 51, 37, 46, 0, -32, -19],
+				[37, 51, 34, 51, 0, -27, -17],
+				[71, 51, 34, 51, 0, -27, -16],
+				[0, 102, 34, 51, 0, -27, -15],
+				[34, 102, 36, 51, 0, -25, -16],
+				[70, 102, 37, 50, 0, -30, -15],
+				[0, 153, 37, 50, 0, -30, -15],
+				[37, 153, 37, 50, 0, -20, -15],
+				[74, 153, 37, 50, 0, -21, -15],
+				[0, 203, 37, 50, 0, -23, -15],
+				[37, 203, 37, 51, 0, -22, -14],
+				[74, 203, 37, 62, 0, -30, -3],
+				[0, 265, 56, 46, 0, -7, -19],
+				[56, 265, 56, 47, 0, -7, -18],
+				[0, 312, 56, 65, 0, -32, 0],
+				[56, 312, 63, 67, 0, -15, -10]
 			],
-            
-            animations: {
-                run: { "frames": [7, 6, 10, 13, 8, 9, 11, 12] },
-				attack: {"frames": [2, 3, 5, 14, 15, 0, 1, 4] , next: "run"
-				}
-            },
+        
+			animations: {
+				"run": { "frames": [5, 8, 3, 0, 6, 7, 1, 2]},
+				"attack": { "frames": [4, 18, 15, 9, 10, 19, 16, 17] , next: "idle"},
+				"idle": { "frames": [11, 12] }
+			},
 		});
-        knight = new createjs.Sprite(spriteSheet, "run");
+        knight = new createjs.Sprite(spriteSheet, "idle");
         knight.y = 35;
 
 	stage.addChild(knight);
-
-	// stage.addEventListener("stagemousedown", attack);
-	window.addEventListener("keydown", movement);
 
 	createjs.Ticker.timingMode = createjs.Ticker.RAF;
 	createjs.Ticker.addEventListener("tick", tick);
@@ -66,22 +67,59 @@ function tick(event) {
 	// knight.y = 200;
 
 	stage.update(event);
+	run()
+	attack()
+	idle()
 }
 
-function movement(event) {
-	let newX = knight.x + 10
-	console.log(event)
-	if (event.key === "ArrowRight"){
-		createjs.Tween.get(knight, {override: true}).to({ x: newX}, 100)
+var pressedSpace = 0
+var pressedRight = 0;
+
+
+function keyPressed(e) {
+	cxc = e.keyCode;
+	console.log(cxc)
+	if (cxc === 39){
+		pressedRight = 1;
+	}
+	else if (cxc === 32){
+		pressedSpace = 1
+	}
+}
+
+function keyUp(e){
+	cxc = e.keyCode;
+	if (cxc === 39){
+		pressedRight = 0;
+	}
+	else if (cxc === 32){
+		pressedSpace = 0
+	}
+}
+
+function run() {
+	if (pressedRight === 1){
+		// stops repeating of animation on continues run
+		if (knight.currentAnimation !== "run"){
+			knight.gotoAndPlay("run");
+		}
+		createjs.Tween.get(knight, {override: true}).to({ x: knight.x + 10}, 100)
 
         createjs.Ticker.addEventListener("tick", stage);
 	}
-	else if (event.code === "Space"){
-		attack()
+
+}
+
+function attack() {
+	if (pressedSpace === 1){
+		if (knight.currentAnimation !== "Space"){
+			knight.gotoAndPlay("attack");
+		}
 	}
 };
 
-function attack() {
-	knight.gotoAndPlay("attack");
+function idle() {
+	if (knight.currentAnimation === "run" && pressedRight === 0){
+		knight.gotoAndPlay("idle");
+	}
 }
-
